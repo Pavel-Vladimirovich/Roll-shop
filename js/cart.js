fc7f43a79667a98c7cc7ae54499580c45188b135
pavel <nesoglasnyj.pavel@gmail.com>
2022-11-02 17:07:09 +0300

    counter дописан. работа над cart 02.10

---
+++ js/cart.js
@@ -0,0 +1,21 @@
+window.addEventListener('click', function (event) {
+
+
+	if (event.target.hasAttribute('data-cart')) {
+		const card = event.target.closest('.card');
+		console.log(card)
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
--- js/counter.js
+++ js/counter.js
@@ -1,18 +1,22 @@
 
 
 window.addEventListener('click', function (event) {
+
+    let counter;// блочная видимость, поэтому объявляем за фигурными скобками
+
+    // проверяем элемент по которому кликнули
+    if(event.target.dataset.action === 'minus' || event.target.dataset.action === 'plus'){
+        
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        counter = counterWrapper.querySelector('[data-counter]');
+    }
+
     if (event.target.dataset.action === 'minus') {
-        let counterWrapper1 = event.target.closest('.counter-wrapper');
-        let counter1 = counterWrapper1.querySelector('[data-counter]');
-        if (parseInt(counter1.textContent) > 1) {
-            counter1.textContent--
+        if (parseInt(counter.textContent) > 1) {
+            counter.textContent--
         }
-
-
     }
     if (event.target.dataset.action === 'plus') {
-        let counterWrapper = event.target.closest('.counter-wrapper');
-        let counter = counterWrapper.querySelector('[data-counter]');
         counter.textContent++
     }
 });
